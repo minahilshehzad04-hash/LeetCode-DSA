5f0592b4c4bad5d1199dff19eb98f16c6ae1ccab
minahilshehzad04-hash <minahilshehzad04@gmail.com>
2026-07-31 21:08:31 +0500

    Time: 1 ms (45.97%) | Memory: 11.1 MB (25.46%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+       stringstream ss(s);
+       string word;
+       string ans="";
+       vector<string> words;
+
+       while(ss >> word){
+        words.push_back(word);
+       }
+        for (int i = words.size() - 1; i >= 0; i--) {
+         ans+=words[i];
+
+         if(i!=0){
+            ans+=" ";
+         }
+        }
+       return ans;
+    }
+};
