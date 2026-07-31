a9f7832dd1ae8784f362ac21826d550fcc2e6702
minahilshehzad04-hash <minahilshehzad04@gmail.com>
2026-07-31 21:32:50 +0500

    Time: 7 ms (24.54%) | Memory: 27.1 MB (87.32%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+       int search(vector<int> & inorder, int left, int right, int val){
+        for(int i=left; i<=right;i++){ 
+            if(inorder[i] == val){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+     TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int& postIdx, int left, int right){
+        if(left > right){
+            return NULL;
+        }
+
+    TreeNode* root = new TreeNode(postorder[postIdx]);
+
+    int inIdx = search(inorder, left, right, postorder[postIdx]);
+    postIdx--;
+    
+    root -> right = helper(inorder,postorder,postIdx, inIdx + 1, right);
+    root -> left = helper(inorder,postorder,postIdx, left, inIdx - 1);
+
+    return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    int postIdx = postorder.size()-1;
+    return helper(inorder,postorder, postIdx, 0, inorder.size()-1);
+
+    }  
+};
