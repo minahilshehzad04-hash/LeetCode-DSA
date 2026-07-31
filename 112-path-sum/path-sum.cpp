71ad7d63e9ff83f8940749ade4f2c90ad1eb0899
minahilshehzad04-hash <minahilshehzad04@gmail.com>
2026-07-31 21:36:33 +0500

    Time: 0 ms (100.00%) | Memory: 21.6 MB (36.91%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL) return false;
+
+        if(root -> left == NULL && root -> right == NULL) {
+           return root->val == targetSum;
+        }
+        int newSum  = targetSum - root -> val;
+
+        bool left = hasPathSum(root -> left, newSum);
+        bool right = hasPathSum(root -> right, newSum);
+
+        return left || right;
+    }
+};
